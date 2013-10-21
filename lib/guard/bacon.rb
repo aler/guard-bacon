7bd655d0e5ea97b3e6f7b777025343c316c7091d
Alex Rudyk <alex.rudyk@gmail.com>
2013-10-21 11:58:50 -0700

    use bacon cmd to run specs
    - remove notifier code
    - remove options code
    - fix template to use spec dir
    - bump version
    - depend on latest guard version

--- lib/guard/bacon.rb
+++ lib/guard/bacon.rb
@@ -12,15 +12,6 @@ module Guard
     def initialize(watchers= [], options= {})
       @last_run_spec = nil
       super
-      if options[:output]
-        puts "Bacon: Using output #{options[:output]}."
-        ::Bacon.extend(::Bacon::const_get(options[:output]))
-      end
-      
-      if options[:backtrace]
-        puts "Bacon: Limiting backtrace to #{options[:backtrace]} lines."
-        ::Bacon.backtrace_size = options[:backtrace]
-      end
     end
 
     def start
@@ -41,55 +32,38 @@ module Guard
     # Called on Ctrl-\ signal
     # This method should be principally used for long action like running all specs/tests/...
     def run_all
-      Dir["specs/**/*_spec.rb"].each do |path|
+      Dir["spec/**/*_spec.rb"].each do |path|
         run_spec(path)
       end
-      
+
       true
     end
-    
+
     def run_spec(path)
       if !File.exists?(path) && @last_run_spec
         puts "spec not found: #{path}"
         puts "running last one."
         path = @last_run_spec
       end
-      
+
       if File.exists?(path)
         @last_run_spec = path
         pid = Kernel.fork do
-          puts "\n●●➤  Running spec: #{path}"
-          counters = ::Bacon.run_file(path)
-          # system "bundle exec bacon -o TestUnit #{path}"
-          # {:installed_summary=>1, :specifications=>19, :depth=>0, :requirements=>30, :failed=>2, :errors => 1}
-          
-          all_specs = counters[:specifications]
-          failed = counters[:failed].to_i + counters[:errors].to_i
-          
-          if failed > 0
-            Notifier.notify("Specs: #{failed} Failures (#{all_specs} specs)",
-                :image => :failed,
-                :title => File.basename(path)
-              )
-          else
-            Notifier.notify("Specs: OK (#{all_specs} specs)",
-                :image => :success,
-                :title => File.basename(path)
-              )
-          end
+          puts "\nRunning spec: #{path}"
+          system "bundle exec bacon -o TestUnit #{path}"
         end
-         
-         Process.wait(pid)
-       else
-         puts "spec not found: #{path}"
-       end
+
+        Process.wait(pid)
+      else
+        puts "spec not found: #{path}"
+      end
     end
-    
+
     def file_changed(path)
       run_spec(path)
       puts ""
     end
-    
+
     # Called on file(s) modifications
     def run_on_changes(paths)
       paths.each do |path|
